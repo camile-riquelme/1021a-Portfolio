379a9cd2bdb68302cfab7d3238730ec11f7a82c7
camile-riquelme <camile.riquelme@estudante.ifms.edu.br>
2023-11-24 20:46:16 -0400

    commit novo

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -41,27 +41,27 @@ export default function Main() {
   const filmesLocais: FilmeType[] = [
     {
       id: 1,
-      titulo: 'Monitoria 2022',
-      sinopse: 'Ficava à disposição para tirar as dúvidas e ajudar alguns alunos com dificuldades. Cada monitoria era realizada em um determinado horário, e somente para uma disciplina determinada.',
-      imagem: '/monitoria.jpg',
+      titulo:'Monitoria 2022',
+      sinopse:'Ficava à disposição para tirar as dúvidas e ajudar alguns alunos com dificuldades. Cada monitoria era realizada em um determinado horário, e somente para uma disciplina determinada.',
+      imagem:'/monitoria.jpg',
     },
     {
       id: 2,
-      titulo: 'Semana do Meio Ambiente 2023',
-      sinopse: 'Participei do Minicurso - Propagação de plantas por métodos assexuados. O curso abordou o tema das técnicas de propagação de plantas via assexuada, tais como: enxertia por gemas, estacas e alporquias.',
-      imagem: '/semana-meio-ambiente.png',
+      titulo:'Semana do Meio Ambiente 2023',
+      sinopse:'Participei do Minicurso - Propagação de plantas por métodos assexuados. O curso abordou o tema das técnicas de propagação de plantas via assexuada, tais como: enxertia por gemas, estacas e alporquias.',
+      imagem:'/semana-meio-ambiente.png',
     },
     {
       id: 3,
-      titulo: 'Semana de Ciência e Tecnologia 2022',
-      sinopse: 'Tiveram palestras, oficinas, mesas-redondas, além das Feiras de Ciência e Tecnologia, e espaços para apresentações de trabalhos científicos.',
-      imagem: '/ciencia-e-tecnologia.png',
+      titulo:'Semana de Ciência e Tecnologia 2022',
+      sinopse:'Tiveram palestras, oficinas, mesas-redondas, além das Feiras de Ciência e Tecnologia, e espaços para apresentações de trabalhos científicos.',
+      imagem:'/ciencia-e-tecnologia.png',
     },
     {
       id: 5,
-      titulo: 'Festival de Arte e Cultura 2022',
-      sinopse: "Houveram palestras, oficinas, minicursos e rodas de conversa. Teve apresentações de trabalhos de estudantes dos ensino fundamental, ensino médio e técnico integrado de nível médio, de escolas públicas e privadas.",
-      imagem: '/arte-e-cultura.jpg',
+      titulo:'Festival de Arte e Cultura 2022',
+      sinopse:'Houveram palestras, oficinas, minicursos e rodas de conversa. Teve apresentações de trabalhos de estudantes dos ensino fundamental, ensino médio e técnico integrado de nível médio, de escolas públicas e privadas.',
+      imagem:'/arte-e-cultura.jpg',
     },
   ];
 
@@ -91,7 +91,6 @@ export default function Main() {
               imagem={filme.imagem}
             />
           ))}
-
 
                 
                 {/* <Filme titulo='Barbie'
